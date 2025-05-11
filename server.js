de7d763b8ba59452c69d51004a9090140f39e68a
Muhammadqodir_Mahmudiy <muhammadqodirhasanov@gmail.com>
2025-05-11 10:03:26 +0500

    connect html

--- server.js
+++ server.js
@@ -1,16 +1,42 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 const server = http.createServer((req, res) => {
   if (req.method === "GET") {
     res.writeHead(200, { "content-type": "text/html charset=utf-8" });
 
-    res.end(`
-        <h2>Send email</h2>
-        <form method="POST" action="/">
-          <input name="name" />
-          <button type="submit">Send</button>
-        </form>
-    `);
+    if (req.url === "/") {
+      fs.readFile(
+        path.join(__dirname, "templates", "index.html"),
+        "utf-8",
+        (err, data) => {
+          if (err) throw err;
+
+          res.end(data);
+        }
+      );
+    } else if (req.url === "/about") {
+      fs.readFile(
+        path.join(__dirname, "templates", "about.html"),
+        "utf-8",
+        (err, data) => {
+          if (err) throw err;
+
+          res.end(data);
+        }
+      );
+    } else if (req.url === "/contact") {
+      fs.readFile(
+        path.join(__dirname, "templates", "contact.html"),
+        "utf-8",
+        (err, data) => {
+          if (err) throw err;
+
+          res.end(data);
+        }
+      );
+    }
   } else if (req.method === "POST") {
     const body = [];
     res.writeHead(200, { "content-type": "text/html; charset=utf-8" });
